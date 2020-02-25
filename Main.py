9665ddc4d520cd9c4f0d441c730a47a840adf1e7
mvaario <mika.vaario@gmail.com>
2020-02-25 16:21:51 +0200

    - Ant on the rubber rope main files

--- Main.py
+++ Main.py
@@ -16,10 +16,10 @@ class settings:
         # Rope's starting length in meters
         self.rope = 10
 
-        # ant's moving speed m/s
+        # ant's moving speed m/s      - light speed = 300 000 km/s
         self.ant_speed = 1
 
-        # Rope's extending speed m/s
+        # Rope's extending speed m/s  - universe expanding speed = 73 km/s
         self.rope_speed = 100
 
         # Rope's acceleration m/s^2
@@ -32,6 +32,7 @@ class settings:
         # Times
         self.sec = 0
         self.min = 0
+        self.full_sec = 0
         # Wait time
         self.time = 0
 
@@ -90,18 +91,47 @@ class settings:
 
 
             if game.sec != sec or game.min != min:
-                print(min, "min", sec, "sec", "- ", round(game.ant_pro, 2), "%")
-
+                if sec == 0:
+                    print(min, "min", sec,"", "sec", "- ", round(game.ant_pro, 2), "%")
+                else:
+                    print(min, "min", sec, "sec", "- ", round(game.ant_pro, 2), "%")
             game.sec = sec
             game.min = min
+
+        game.full_sec = game.full_sec + 1
+
+
         return
 
+    # Calculations
+    def cal():
+
+        dis = 10
+        ant_speed = 1
+        rope_speed = 10
+        acc = 0
+
+
+        # dis * ant_speed * t = dis * t * rope_speed * acc
+        t = game.full_sec
+        ant = dis * ant_speed * t
+        rope = dis * rope_speed * t
+
+        print("")
+        print(ant)
+        print(rope)
+
+
+
+
+        return
 if __name__ == '__main__':
 
     game = settings()
     last_time = time.time()
     i = 0
     F = 0
+    kk = 0
     print("Starting")
     print("---------------")
     print("Time - Process")
@@ -110,15 +140,21 @@ if __name__ == '__main__':
         i = i + 1
         settings.start()
         F = game.ant_pro
-        settings.time(i)
+        if i % 2592000 == 0:
+            settings.time(i)
+            kk = kk + 1
 
+    settings.time(i)
+    settings.cal()
     # Final results
-    time.sleep(0.2)
+    # time.sleep(0.2)
     print("")
     print("Done, final data")
     print("---------------")
     print("Time", game.min, "min", game.sec, "sec")
     print("Ant final position", round(game.ant, 2), " m")
+    print("Ant moved ", round(game.ant_speed * game.full_sec), "m")
     print("Rope final length", round(game.rope, 2), " m")
+    print("Kuukausia ", kk)
 
 
